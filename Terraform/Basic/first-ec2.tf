4b2453bcaab297b53a15b2f8ab09e8c81d646914
jigisha29 <jigisha.araniya29@gmail.com>
2021-10-11 18:46:59 +0530

    Update first-ec2.tf
    new update

--- Terraform/Basic/first-ec2.tf
+++ Terraform/Basic/first-ec2.tf
@@ -3,12 +3,12 @@ provider "aws"{
   region = "us-east-1"
 }
 resource "aws_instance" "myfirstec2"{
-  ami = "ami-0b679d9e424a41e40"
+  ami = "ami-0b679d9e424456342"
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-b17df5c6"]
-  key_name = "jigi-nv-key"
+  vpc_security_group_ids = ["sg-b17df6ct"]
+  key_name = "my-nv-key"
   tags = {
-    Name = "Scoute Suite demo"
+    Name = " Mydemo"
   }
 
 }
